9557702e3abc6fef87afb0575af251ab37c5724a
iCoder0020 <ishansang1999@gmail.com>
2020-02-20 19:49:38 +0530

    create all DBs

--- try_django/arista/api/models.py
+++ try_django/arista/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
+#TO-FIX: on_delete = models.CASCADE everywhere for now
+
 class User(models.Model):
     user_id = models.AutoField(primary_key = True)
     user_name = models.CharField(max_length = 20)
@@ -24,11 +25,51 @@ class Group(models.Model):
 
 
 class Group_User(models.Model):
-    user_id = models.ForeignKey('User', on_delete = models.CASCADE)
-    group_id = models.ForeignKey('Group', on_delete = models.CASCADE)
+    user = models.ForeignKey('User', on_delete = models.CASCADE)
+    group = models.ForeignKey('Group', on_delete = models.CASCADE)
 
     class Meta:
-        unique_together = (("user_id","group_id"))
+        unique_together = (("user", "group"))
 
     def __str__(self):
-        return (str(self.user_id), str(self.group_id))
+        return str(self.group) + ", " + str(self.user)
+
+# class Friends(models.Model):
+#     group = models.ForeignKey('Group', on_delete = models.CASCADE, primary_key = True)
+#     user1 = models.ForeignKey('User', related_name = "User1", on_delete = models.CASCADE)
+#     user2 = models.ForeignKey('User', related_name = "User2", on_delete = models.CASCADE)
+
+#     def __str__(self):
+#         return str(self.user1) + ", " + str(self.user2)
+
+class Payment_Whole(models.Model):
+    payment_id = models.AutoField(primary_key = True)
+    group = models.ForeignKey('Group', on_delete = models.CASCADE)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+class Payment_Individual(models.Model):
+    payment = models.ForeignKey('Payment_Whole', on_delete = models.CASCADE)
+    lender = models.ForeignKey('User', on_delete = models.CASCADE)
+    amount = models.FloatField()
+
+    class Meta:
+        unique_together = (("payment", "lender"))
+
+    def __str__(self):
+        return str(self.payment) + ", " + str(self.lender)
+
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key = True)
+    payment = models.ForeignKey('Payment_Whole', on_delete = models.CASCADE)
+    lender = models.ForeignKey('User', related_name = "Lender", on_delete = models.CASCADE)
+    borrower = models.ForeignKey('User', related_name = "Borrower", on_delete = models.CASCADE)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return str(self.payment) + ": " + str(self.borrower) + "->" + str(self.lender)
+
